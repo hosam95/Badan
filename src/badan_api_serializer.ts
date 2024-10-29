c79a6792758988eb80f3996b1b2801b3a9f04f85
hosam95 <hosamsaeid95@gmail.com>
2024-10-29 17:46:06 +0300

    implement a badan-api-serializer abstract class

---
+++ src/badan_api_serializer.ts
@@ -0,0 +1,29 @@
+import { RequestHandler, Responder } from "types";
+
+export abstract class BadanApiSerializer{
+
+    abstract url:string;
+    abstract method:"Get"|"Post"|"Put"|"Delete"|"Patch";
+    abstract description:string;
+
+
+    abstract responder:Responder;
+
+    abstract handler:(...inputs: any[])=>void
+
+    abstract Controller: RequestHandler
+
+    abstract Logic: RequestHandler
+    
+    abstract validateInput: RequestHandler
+
+    abstract generateDocumentationMD:()=>void
+    
+    
+    require_auth:boolean=false;
+    allowed_roles:Array<string>=[]
+
+    abstract authenticate: RequestHandler;
+
+    abstract roleAuthorization: RequestHandler;
+}
