705042432d8b23684e64034dc43ceeb07ee2d227
hosam95 <hosamsaeid95@gmail.com>
2024-10-29 15:54:54 +0300

    add a user parameter to the RequestData & implement a BadanPipe type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,11 @@
-export interface RequestData{
+import { Respond } from "badan-serializers";
+
+export interface RequestData<T = any>{
     body:any;
     params:any;
     query:any;
     headers:any;
+    user?:T
 }
+
+export type BadanPipe= Array<(req:RequestData,res:Respond,...next:BadanPipe)=>void>
