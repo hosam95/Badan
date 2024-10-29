73017f5b5520f5b897a627d6c4cbc146e0418808
hosam95 <hosamsaeid95@gmail.com>
2024-10-29 18:00:12 +0300

    implement the setListener method to the badan-expresser

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { BadanCoreSerializer, Respond, Use, Responder } from "badan-serializers";
+import { BadanCoreSerializer, Respond, Use, Responder, BadanApiSerializer } from "badan-serializers";
 import { RequestHandler, Response, Application, Request } from 'express'
 
 export class BadanExpresser extends BadanCoreSerializer{
@@ -11,6 +11,31 @@ export class BadanExpresser extends BadanCoreSerializer{
     user(app: any): Use {
         return (...handlers: RequestHandler<any, any, any, any, Record<string, any>>[])=>{
             app.use(handlers)
+        }
+    }
+
+    setListener(app:Application,api:BadanApiSerializer){
+        switch(api.method){
+            case "All":
+                app.all(api.url,api.handler)
+                break;
+            case "Get":
+                app.get(api.url,api.handler)
+                break;
+            case "Post":
+                app.post(api.url,api.handler)
+                break;
+            case "Put":
+                app.put(api.url,api.handler)
+                break;
+            case "Delete":
+                app.delete(api.url,api.handler)
+                break;
+            case "Patch":
+                app.patch(api.url,api.handler)
+                break;
+            default:
+                throw(Error(`invalid requiste method: '${api.method}' at ${api.constructor.name}`));
         }
     }
 }
