1169b454a9b517df30c962345ee182038e340ea0
hosam95 <hosamsaeid95@gmail.com>
2024-10-29 17:26:52 +0300

    clean the application class code and update the listen method

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
-import { BadanAuthSerializer, BadanCoreSerializer } from "badan-serializers";
+import { Application, Use } from "badan-serializers";
 import { BadanModule } from "./module/badan_module.js";
-import { Application, BadanExpresser, Use } from "badan-expresser";
-import { BadanDefaultAuthenticator } from "default_authenticator.js";
-import { BadanDefaultCoreSerializer } from "default_core.js";
 
 /**@todo: seperate express from this layer */
 export class Badan extends BadanModule{
     app:Application;
     
     constructor(app:Application,options?:{}){
-        //expect a badan-authenticator, badan_core_serializer  in options
+        /**@todo: expect a badan-authenticator, badan_core_serializer  in options*/
         super()
         this.app=app;
         this.use=this.coreSerializer.user(this.app)
@@ -17,7 +14,7 @@ export class Badan extends BadanModule{
 
 
     listen(port:number=4000){
-        this.subModules.map(module=>module.setAPIsListeners(this.app))
+        this.setAllListeners(this.app)
         return this.app.listen(port)
     }
 
