9e28ad309f387793638fefacc57fb3420e7df494
hosam95 <hosamsaeid95@gmail.com>
2024-10-29 16:14:45 +0300

    clean the standaredApi class & use an express-pipe like pipe & remove the Auth logic

--- src/standard_api/standard_api.ts
+++ src/standard_api/standard_api.ts
@@ -1,19 +1,18 @@
-import { RolesUnion, User } from '../../../test/models/user.js'; /**@todo: include to the package */
-import Jwt from 'jsonwebtoken';
+
 import typia from 'typia';
 import { schema_to_json } from '../DocGen/typia_schema_parser.js';
 
-import { Respond } from 'badan-expresser'
-import { RequestData } from 'types.js';
+import { RequestHandler, Respond, Responder } from 'badan-serializers'
+import { BadanPipe, RequestData } from 'types.js';
 
 /**@todo: consider writing the input validator by your self using the input schemas to avoid redundancy but check the performance in comparison to typia */
-/**@todo: consider migrating the standard-api logic to an express-pipe like pipe with the (req,res) parser at it's beginning */
 
 
 export abstract class StandardApi{
+    private pipe:BadanPipe=[this.Controller,this.Logic];
+
     require_auth:boolean=false;
-    allowed_roles:Array<RolesUnion>=[]
-    description:string=''
+    allowed_roles:Array<string>=[]
 
 
     query_validator:(input:unknown)=>typia.IValidation<any>=typia.createValidate<any>()
@@ -21,72 +20,33 @@ export abstract class StandardApi{
 
     query_schema?=typia.llm.schema<any>()
     body_schema?=typia.llm.schema<any>()
+    
     abstract url:string;
     method:"Get"|"Post"|"Put"|"Delete"|"Patch"='Get'
+    description:string=''
 
-    Responder:(res:any)=>Respond=(res:any)=> (status:number,response:any)=>{};
+
+    responder:Responder = (res:any)=>(status:number,response:any)=>{};
 
     constructor(){
         this.handler=this.handler.bind(this);
     }
     
-    handler(req:Request,res:Response){
+    handler(req:any,res:Response){
         
-        //calls the auth checker
-        // parse the Express(req,res) to Badan(req,respond)
-        this.authenticate(req,this.Responder(res));
+        // parse the (req,res) of Express to (req,respond) of Badan
+        this.pipe[0](req as RequestData, this.responder(res),...this.pipe.slice(1));
     }
 
-    Controller(req:RequestData,respond:Respond){
+    Controller(req:RequestData, respond:Respond,...next:BadanPipe){
 
-        this.Logic(req,respond)
+        next[0](req,respond,...next.slice(1))
     }
 
     Logic(data:RequestData,respond:Respond){}
     
-    async authenticate(req:any,respond:Respond){
-        if(!this.require_auth){
-            this.validateInput(req,respond)
-            return
-        }
-        
-        let token= req.headers['authorization']
-
-        if(token === null){
-            respond(401,{"message":"authorization required"})
-            return
-        }
-
-        Jwt.verify(token,process.env.ACCESS_TOKEN_KEY as string,(err:any,user:any)=>{
-            if(err){
-                respond(401,{"error":err})
-                return
-            }
-
-            req.user=new User(user)
-            this.roleAuthorization(req,respond);
-        })
-
-    }
-
-    roleAuthorization(req:any,respond:Respond){
-        
-        if(req.user.roles.includes("admin")||this.allowed_roles.length==0){
-            this.validateInput(req,respond);
-            return;
-        }
-
-        for (let role in this.allowed_roles){
-            if ((req.user.roles as Array<RolesUnion>).includes(role)){
-                this.validateInput(req,respond);
-                return;
-            }
-        }
-        
-        respond(403,{"error":"authorization failed, you dont have access to this api"})
-    }
     
-    validateInput(req:any,respond:Respond){
+    validateInput(req:any,respond:Respond, ...next:BadanPipe){
         let queryValidation=this.query_validator(req.query)
         let bodyValidation=this.body_validator(req.body)
 
@@ -95,7 +55,8 @@ export abstract class StandardApi{
             return;
         }
 
-        this.Controller(req,respond);
+        next[0](req,respond,...next.slice(1));
+
     }
 
     generateDocumentationMD(){
@@ -110,4 +71,7 @@ export abstract class StandardApi{
         
         return `## ${this.constructor.name} \n${this.description}\n\n#### Url: *${this.method}* ${'`'+this.url+'`'}\n\n${(req_body? "#### Body:\n"+"```\n"+req_body+"\n```":"")}\n\n${(req_query? "#### Query:\n"+"```\n"+req_query+"```\n":"")}---\n` 
     }
+
+    authenticate: RequestHandler=(req:any,respond:Respond,...next:RequestHandler[])=>{};
+    roleAuthorization: RequestHandler= (req:any,respond:Respond,...next:RequestHandler[])=>{};
 }
