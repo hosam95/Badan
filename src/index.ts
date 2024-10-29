a8a834aeacb55dfa179f33febaa6db44c80f1d22
hosam95 <hosamsaeid95@gmail.com>
2024-10-29 16:03:23 +0300

    remove the sub-modules logic from the Badan class and extend it from the BadanModule instead

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
+import { BadanAuthSerializer, BadanCoreSerializer } from "badan-serializers";
 import { BadanModule } from "./module/badan_module.js";
 import { Application, BadanExpresser, Use } from "badan-expresser";
+import { BadanDefaultAuthenticator } from "default_authenticator.js";
+import { BadanDefaultCoreSerializer } from "default_core.js";
 
 /**@todo: seperate express from this layer */
 export class Badan extends BadanModule{
     app:Application;
-    modules:BadanModule[]=[]
-    coreSerializer=new BadanExpresser()
-
-    constructor(app:Application){
+    
+    constructor(app:Application,options?:{}){
+        //expect a badan-authenticator, badan_core_serializer  in options
         super()
         this.app=app;
         this.use=this.coreSerializer.user(this.app)
     }
 
-    appendModule(module:BadanModule){
-        this.modules.push(module);
-    }
 
     listen(port:number=4000){
-        this.modules.map(module=>module.setAPIsListeners(this.app))
+        this.subModules.map(module=>module.setAPIsListeners(this.app))
         return this.app.listen(port)
     }
 
