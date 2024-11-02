fe01442f250cb2efe780b4ffa2ae77ed00306dee
hosam95 <hosamsaeid95@gmail.com>
2024-11-02 20:01:21 +0200

    serializer bug fixes

--- packages/serializers/src/index.ts
+++ packages/serializers/src/index.ts
@@ -1,7 +1,7 @@
-export * from "badan_core_serializer";
-export * from 'badan_auth_serializer';
-export * from 'badan_api_serializer';
-export * from 'database_serializer';
+export * from "./badan_core_serializer.js";
+export * from './badan_auth_serializer.js';
+export * from './badan_api_serializer.js';
+export * from './database_serializer.js';
 
-export * from "types";
-export * from "application";
+export * from "./types.js";
+export * from "./application.js";
