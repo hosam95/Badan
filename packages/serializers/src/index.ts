54c6f99a1c859dd2b459e65936a744cdd42e2656
hosam95 <hosamsaeid95@gmail.com>
2024-11-04 15:10:36 +0200

    add http as dependency & upgrade to latest @types/node version & set the module and types fields of the package.json & update the version

--- packages/serializers/src/index.ts
+++ packages/serializers/src/index.ts
@@ -3,6 +3,5 @@ export * from './badan_auth_serializer.js';
 export * from './badan_api_serializer.js';
 export * from './database_serializer.js';
 
-
 export * from "./types.js";
 export * from "./application.js";
